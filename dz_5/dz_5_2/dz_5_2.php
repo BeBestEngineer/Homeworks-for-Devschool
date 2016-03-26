4011c849c265890e6056b2a3ddd34a906160c106
Alek_Gorchakov <gorchakov-aleksandr@lenta.ru>
2016-03-26 18:37:45 +0600

    dz_5_2 deb_7

--- dz_5/dz_5_2/dz_5_2.php
+++ dz_5/dz_5_2/dz_5_2.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-type: text/html; charset=utf-8');   
-error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE);
+error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE|E_ALL);
     ini_set('display_errors', 1);
 
 header('Content-type: text/html; charset=utf-8');

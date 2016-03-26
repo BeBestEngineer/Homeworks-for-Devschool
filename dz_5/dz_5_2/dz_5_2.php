53245369f86bb74a43bb7d79c8e5cd97376fb9a3
Alek_Gorchakov <gorchakov-aleksandr@lenta.ru>
2016-03-26 18:33:39 +0600

    dz_5_2 deb_6

--- dz_5/dz_5_2/dz_5_2.php
+++ dz_5/dz_5_2/dz_5_2.php
@@ -1,5 +1,6 @@
 <?php
-   error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE);
+header('Content-type: text/html; charset=utf-8');   
+error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE);
     ini_set('display_errors', 1);
 
 header('Content-type: text/html; charset=utf-8');
@@ -14,42 +15,41 @@ $news='Четыре новосибирские компании вошли в с
 Звезды телешоу «Голос» Наргиз Закирова и Гела Гуралиа споют в «Маяковском»';
 $news = explode("\n", $news);
 
-var_dump ( $_POST );
-
 # Точка входа
-if (count($_POST) == 0 || strlen($_POST['id']) == 0) {
-    echo '<i>Введите знак \'#\' чтобы просмотреть список новостей</i>';
-} elseif ($_POST['id'] == '#') {
-    All_news ();
-} elseif (preg_match('@[0-8]@u', $_POST['id']) && strlen($_POST['id']) <= 1) {
-    Specific_news ();
+if ( isset( $_POST['id'] ) ) {
+    Check_id ( $news );    
 } else {
-    echo '<h1>Ошибка 404</h1>';
     header("HTTP/1.0 404 Not Found");
+    include_once "404.html";    
 }
-        
+
+# Функция проверки значения параметра id:
+function Check_id ( $n ) {
+if ( $_POST['id'] >= 0 && $_POST['id'] <= ( count( $n ) - 1 ) && !preg_match('@[ \D ]@u', $_POST['id']) && $_POST['id'] !=='' ) {
+    Specific_news ( $n, $_POST['id'] );
+} else {
+    All_news ( $n );
+  }
+}
+
 # Функция вывода всего списка новостей:
-function All_news () {
-    global $news;
-    
-    echo '<h4>Список новостей (для выбора новости введите в адресную строку параметр news_number=0...8):</h4>';
-        foreach ( $news as $key => $value ) {
+function All_news ( $n ) {
+    echo '<h4>Список новостей (для выбора новости введите в текстовое поле цифру от 0 до 8:</h4>';
+        foreach ( $n as $key => $value ) {
             echo ''.$key.'. '.$value.'';
             echo '<br>';
     }
-}        
+}
 
 # Функция вывода конкретной новости:
-function Specific_news () {
-    global $news;
-    
-    echo ''.$_POST['id' ].'. ';
-    echo $news [ $_POST['id' ] ];
+function Specific_news ( $n, $pid ) {
+    echo ''.$pid.'. ';
+    echo $n [ (int)$pid ];
     echo '<br>';
-    echo '<i>Чтобы венрнуться к списку новостей введите в <strong>текстовое поле</strong> знак \'#\'</i>';
+    echo '<br>';
+    echo ' &nbsp  &nbsp <i>Для вывода списка новостей введите параметр \'all\' в текстовое поле.</i>';
 }
 ?>
-
 
 <!DOCTYPE HTML>
 <html>
@@ -59,7 +59,7 @@ function Specific_news () {
  </head>
  <body>
 
- <form action="index.php", method ="POST">
+ <form action="dz_5_2.php", method ="POST">
   <p><b>Текстовое поле</b></p>
   <p>
   <input type="text" name="id" value="">
